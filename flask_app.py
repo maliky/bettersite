f704051a21e0593cda22405c18cca32743d579df
mkone@paw <malikykone@gmail.com>
2022-02-10 15:07:44 +0000

    étape 1, structure de la page admin

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,16 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    return render_template('admin.html')
+
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask!'
+    return 'Hello from  bettersite!'
 
 # Ceci est un commentaire pour la fin de mon fichier flask_app.py dans la branch dev
