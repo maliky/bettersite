a48f93b178c03efe2662f18aec37052a19da56bf
mlkbk <malikykone@gmail.com>
2022-03-08 20:59:41 +0000

    add basic files

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,20 @@
-
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    clients = [
+        {"nom":"Areta F.", "mtp": "ABC"},
+        {"nom":"Ernesto Djé djé", "mtp": "1XB"},
+        {"nom":"Eminem", "mtp": "123"},
+        ]
+    return render_template('admin.html', clients = clients)
+
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask!'
+    return 'Hello from  bettersite!'
 
+# Ceci est un commentaire pour la fin de mon fichier flask_app.py dans la branch dev
