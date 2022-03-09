8fa22bcebf53e805baed2bdeb989320b57c515a3
mlkbk <malikykone@gmail.com>
2022-03-09 18:31:46 +0000

    ajoute la logique pour ajouter un utilisateur au fichier

--- flask_app.py
+++ flask_app.py
@@ -1,26 +1,56 @@
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, render_template
-from chiffrement import charge_fichier_mtp
+from flask import Flask, render_template, request
+from chiffrement import charge_fichier_mtp, est_dans_db, crypte_mtp
 import os
 
 app = Flask(__name__)
+NOM_BD = "bd.test.tsv"
+MASTER_KEY = "lRAufiLOsaycti5e2XHecZeqEj00H6kcCm_yQdqcSH4="
 
-@app.route('/admin', methods=['GET', 'POST'])
+
+@app.route("/admin", methods=["GET", "POST"])
 def admin():
-    nom_bd = "bd.test.tsv"
-    if os.path.exists(nom_bd):
-        clients = charge_fichier_mtp(nom_bd)
+    if os.path.exists(NOM_BD):
+        clients = charge_fichier_mtp(NOM_BD)
     else:
         clients = []
-    return render_template('admin.html', clients = clients)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from  bettersite!'
+    if request.method == "POST":
+        nom = request.form["client"]
+        mtp = request.form["mtp"]
+        nv_client = {"nom": nom, "mtp": mtp}
+        
+        if not est_dans_db(nv_client, nom_fichier=NOM_BD, key=MASTER_KEY):
+            # ajouter un client
+
+            nv_client_chiffre = crypte_mtp(nv_client, key=MASTER_KEY)
+            clients.append(nv_client_chiffre)
+
+            return render_template("admin.html", clients=clients)
+
+    # si c'est GET
+    return render_template("admin.html", clients=clients)
+
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    """Page de login"""
+    if request.method == "GET":
+        return render_template("login.html")
+
+    # vérifie les données saisie
+    nom = request.form["client"]
+    mtp = request.form["mtp"]
+    clients = charge_fichier_mtp(NOM_BD)
+
+    if est_dans_db(nom, nom_fichier=NOM_BD, key=MASTER_KEY):
+        return render_template("produits.html")
+
+    return render_template("login.html", message="utilisateur ou mot de passe inconnu.")
+
 
 # Ceci est un commentaire pour la fin de mon fichier flask_app.py dans la branch dev
-
 
 
 if __name__ == "__main__":
