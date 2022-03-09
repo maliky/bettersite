d45e2edc522f4da87eb60ce9444c0754ba9da9d2
mkone@paw <malikykone@gmail.com>
2022-03-09 12:03:55 +0000

    tableau clients affichés

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
+from chiffrement import charge_fichier_mtp
+import os
 
 app = Flask(__name__)
 
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
-    return render_template('admin.html')
+    nom_bd = 'bettersite/bd.test.tsv'
+
+    if os.path.exists(nom_bd):
+        clients = charge_fichier_mtp(nom_bd)
+        return render_template('admin.html', clients=clients)
+
+    abort(404)
 
 @app.route('/')
 def hello_world():
