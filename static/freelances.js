80753f9542a4b7de0116f8a5234c75773e11cc7a
mlkbk <malikykone@gmail.com>
2022-03-15 20:30:29 +0000

    structure de base javascript pour débuggage

--- static/freelances.js
+++ static/freelances.js
@@ -1,7 +1,35 @@
+const cartes = document.querySelectorAll('.carte');
 
-function test() {
+/*
+   map est une façon plus claire et succinte de faire un boucle
+   Cela résume
+   for (let i=0; i<cartes.length; i++){
+   var carte = cartes[i];
+   carte.addEventListener("mouseover", rendre_interactive)
+   }
+ */
+
+cartes.forEach(rendre_interactive);
+
+// on ajoute sur chaque carte des events listener
+function rendre_interactive(carte) {
+    carte.addEventListener('mouseover', mettre_en_surbrillance);
+    carte.addEventListener('mouseout', supprimer_la_surbrillance);
+    carte.addEventListener('click', garder_la_surbrillance);
     debugger;
-    let cartes = document.querySelector('.carte');
-    alert('Ça marche !');
-    console.log(cartes);
+};
+
+// on définie les fonctions des events listener
+// https://developer.mozilla.org/en-US/docs/Web/Events
+               
+function mettre_en_surbrillance(obj){
+                   debugger;
+}
+
+function supprimer_la_surbrillance(obj){
+    debugger;
+}
+
+function garder_la_surbrillance(obj){
+    debugger;
 }
